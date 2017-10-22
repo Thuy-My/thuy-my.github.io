3a59b3d2bd23d18424d89262f25d9315f7a5912f
Thuy-My <thuy-my.tran@epfl.ch>
2017-10-22 19:55:10 +0200

    Delete data files that were outside the folder

--- _site/src/charts.js
+++ _site/src/charts.js
@@ -1,17 +1,19 @@
 let currChart;
 let currChart2;
-let firstClick = true;
 
 /* Function called when a username has been clicked */
 function usernameClicked(username) {
     document.getElementById("chosenUser").innerHTML = username;
 
+    /* Set all the following objects to be visible */
     document.getElementById("fineToothComb").style.display = "block";
     document.getElementById("sticky2").style.display = "block";
     document.getElementById("sticky3").style.display = "block";
 
     drawMostActiveRepoChart(username);    
     drawLanguagesChart(username);
+
+    analyze(username);
 
     return false;
 }
@@ -30,7 +32,7 @@ function drawMostActiveRepoChart(username) {
     let url = "https://thuy-my.github.io/data/names_" + username +".txt";
     let url2 = "https://thuy-my.github.io/data/values_" + username + ".txt";
 
-    //let url = "http://localhost:4000/data/repo_" + username +".txt";
+    //let url = "http://localhost:4000/data/names_" + username +".txt";
     //let url2 = "http://localhost:4000/data/values_" + username + ".txt";
 
     let namesData;
@@ -148,6 +150,16 @@ function drawDoughnutChart(labelsFile, valuesFile, label, ctx, canvasName) {
         bColor.push('rgba(' + r + ", " + g + ", " + b + ", 1");
     }
 
+    /* To compute each language percentage */
+    let fullPercentage = 0.0;
+    for(let j = 0; j < values.length; j++) {
+        fullPercentage += parseInt(values[j]);
+    }
+
+    for(let k = 0; k < values.length; k++) {
+        values[k] = ((parseInt(values[k]) * 1.0 / fullPercentage) * 100).toFixed(3);
+    }
+
     let doughnutChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
@@ -196,6 +208,95 @@ function getFile(url, func) {
     namesFile.send();
 }
 
-function jsonToCSV() {
+function analyze(username) {
+
+    let url = "https://thuy-my.github.io/data/names_" + username +".txt";
+    let url2 = "https://thuy-my.github.io/data/values_" + username + ".txt";
+
+    let repoData;
+    let repoValues;
+
+    getFile(url, (repoData) => {
+        getFile(url2, (repoValues) => {
+            computeMedian(username, repoData, repoValues, "repos");
+        })
+    });
+
+    let url3 = "https://thuy-my.github.io/data/languages_" + username +".txt";
+    let url4 = "https://thuy-my.github.io/data/languagesValues_" + username + ".txt";
+
+    let languagesData;
+    let languagesValues;
+    getFile(url3, (languagesData) => {
+        getFile(url4, (languagesValues) => {
+            computeMedian(username, languagesData, languagesValues, "languages");
+        })
+    });
 
 }
+
+function computeMedian(username, names, values, choice) {
+    let namesArray = names.split(',');
+    let valuesArray = values.split(',');
+
+    let middle = 0.0;
+    let totalCommits = 0;
+    let length = valuesArray.length;
+
+    for(let i = 0; i < length; i++) {
+        middle += parseFloat(valuesArray[i]);
+    }
+
+    totalCommits = middle; // Total commits count
+    middle = Math.ceil(middle / 2);
+    
+    let index = 0;
+    let total = 0.0;
+    while(total < middle) {
+        total += valuesArray[index];
+        index++;
+    }
+
+    let average = (totalCommits * 1.0 / length).toFixed(2);    // The theorical number of commits per repository 
+    
+    /* Get the names of the very most commited repos */
+    let topMost = [];
+    for(let j = 0; j < index; j++) {
+        topMost.push(namesArray[j]);
+    }
+
+    if(choice == "repos") {
+
+        document.getElementById("insideSticky2").innerHTML =
+            "<tt>" + username + " has " + length + " (public) repositories with a total commits count of " + totalCommits + ".<br>" +
+            "The average number of commits per repository would be " + average + ".</tt>";   
+        
+        /* To manage singular and plural sentences */
+        if(index == 1) {
+            document.getElementById("insideSticky2_2").innerHTML = 
+                "<tt>But the first " + index + " repository contains more commits than all the others combined! " +
+                "His/her first " + index + " repository (which is : " + topMost.join() + ") must be dear to his/her heart. :)</tt>";
+        } else {
+            document.getElementById("insideSticky2_2").innerHTML = 
+                "<tt>But the first " + index + " repositories contains more commits than all the others combined! " +
+                "His/her first " + index + " repositories (which are : " + topMost.join() + ") must be dear to his/her heart. :)</tt>";
+        }
+
+    } else if(choice == "languages") {
+
+        document.getElementById("insideSticky3").innerHTML = 
+            "<tt><small><i>Note : The adjacent chart values are represented in percentage.</i></small><br><br>"; 
+
+        if(index == 1) {
+            document.getElementById("insideSticky3_2").innerHTML =
+                "<tt>" + username + " must be a monoglot! He/She has used " + index + " language (" + topMost.join() + ") more than the rest of them combined. " +
+                "Maybe someone should tell him/her to pay more attention to the other languages too, it is nice to be a polyglot. :D</tt>";
+        } else {
+            document.getElementById("insideSticky3_2").innerHTML =
+                "<tt> Seeing those results, " + username + " must be a polyglot! He/She has used " + index + " languages (" + topMost.join() + ") more " +
+                "than the rest of them combined. Kudos!</tt>";
+        }
+
+    }
+}
+
